1700c4592eb00143cf59d0acb28247522a744bcf
AlexanderMtchedlidze <mtchedlidzealexander@gmail.com>
2023-04-21 22:26:59 +0400

    feat(registration tests): add registration tests

--- app/Http/Requests/StoreRegisterRequest.php
+++ app/Http/Requests/StoreRegisterRequest.php
@@ -25,7 +25,7 @@ class StoreRegisterRequest extends FormRequest
 		return [
 			'name'            => ['required', 'min:3', 'max:255', Rule::unique('users', 'name')],
 			'email'           => ['required', 'email', 'max:255', Rule::unique('users', 'email')],
-			'password'        => ['required', 'min:8', 'max:255', 'confirmed'],
+			'password'        => ['required', 'min:3', 'max:255', 'confirmed'],
 		];
 	}
 }

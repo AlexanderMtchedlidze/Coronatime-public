2bfbb3a575550519ca5d4faad45eb851b73387bf
AlexanderMtchedlidze <mtchedlidzealexander@gmail.com>
2023-04-21 19:45:21 +0400

    feat(lang middleware tests): add cases for updating and checking lang middleware

--- app/Http/Controllers/LangController.php
+++ app/Http/Controllers/LangController.php
@@ -6,6 +6,9 @@ class LangController extends Controller
 {
 	public function __invoke($lang)
 	{
-		return back()->cookie('lang', $lang, 60 * 24 * 30);
+		if ($lang === 'en' || $lang === 'ka') {
+			return back()->cookie('lang', $lang, 60 * 24 * 30);
+		}
+		return back()->withErrors(['lang' => 'Invalid language code ' . $lang]);
 	}
 }

d7be88059549ea4196b5862c2856dc28732ff3e0
AlexanderMtchedlidze <mtchedlidzealexander@gmail.com>
2023-04-21 20:34:18 +0400

    feat(command test): add command test

---
+++ tests/Feature/FetchDataCommandTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+
+class FetchDataCommandTest extends TestCase
+{
+	use RefreshDatabase;
+
+	public function test_data_is_saved_in_database_after_running_a_command(): void
+	{
+		$this->artisan('app:fetch-data-command')->assertExitCode(0)->expectsOutput('Data fetched and saved successfully!');
+		$count = DB::table('countries')->count();
+		$this->assertGreaterThan(0, $count);
+	}
+}

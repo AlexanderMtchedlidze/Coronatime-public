0c9a0b7d1c8eea9c5a53af632f91db5a504e5ae5
AlexanderMtchedlidze <mtchedlidzealexander@gmail.com>
2023-04-23 13:43:41 +0400

    feat(email): extract custom verification into mail

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use App\Mail\EmailVerification;
 use App\Notifications\ResetPassword;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\URL;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements MustVerifyEmail
@@ -46,6 +49,20 @@ class User extends Authenticatable implements MustVerifyEmail
 	public function setPasswordAttribute($password)
 	{
 		$this->attributes['password'] = bcrypt($password);
+	}
+
+	public function sendEmailConfirmationNotification()
+	{
+		$verificationUrl = URL::temporarySignedRoute(
+			'verification.verify',
+			now()->addMinutes(60),
+			[
+				'id'   => $this->id,
+				'hash' => sha1($this->email),
+			]
+		);
+
+		Mail::to($this->email)->send(new EmailVerification($verificationUrl));
 	}
 
 	/**

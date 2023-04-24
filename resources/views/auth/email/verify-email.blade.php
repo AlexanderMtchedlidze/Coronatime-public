9dbfa1f75dc9fa3af717a4b470edfe6978f5a1de
AlexanderMtchedlidze <mtchedlidzealexander@gmail.com>
2023-04-24 23:28:11 +0400

    feat(emails): extract email specific component

--- resources/views/auth/email/verify-email.blade.php
+++ resources/views/auth/email/verify-email.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <x-_email-header>
+    <x-_email-notice-header>
         <x-slot:heading>{{ trans('email.feedback') }}</x-slot:heading>
-    </x-_email-header>
+    </x-_email-notice-header>
 </x-layout>
--- resources/views/components/_email-header.blade.php
+++ resources/views/components/_email-header.blade.php
@@ -1,11 +1,27 @@
-<x-_reset-header>
-    <div class="flex flex-col items-center justify-center gap-5 mt-16">
-        <img src="{{ asset('icons/icons8-checkmark.gif') }}" alt="Checkmark gif">
-        <p class="text-base md:text-lg text-center">
-            {{ $heading }}
-        </p>
+@props(["url"])
 
-        {{ $slot }}
+<!DOCTYPE html>
+<html lang="en" style="height: 100vh">
+<head>
+    <meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;900&display=swap" rel="stylesheet">
+</head>
+<body style="font-family: Inter, sans-serif; height: 100vh; display: grid; place-items: center">
+<main>
+    <div style="margin: 0 auto; max-width: 32.5rem">
+        <img src="https://iili.io/H8AIa1I.png" alt="Landing page screenshot"
+             style="max-width: 100%">
     </div>
-</x-_reset-header>
-
+    <div style="text-align: center">
+        <h1 style="font-size: 1.5rem; font-weight: bold">{{ $heading }}</h1>
+        <p style="font-size: 1.1rem; margin-top: 2.4rem">{{ $subheading }}</p>
+    </div>
+    <div style="text-align: center; margin-top: 1rem;">
+        <a href="{{ $url }}"
+           style="padding: 1rem 7rem; font-size: 1rem; text-decoration: none; background-color: #0FBA68; color: #FFF; border-radius: 0.5rem; font-weight: bold">{{ $slot }}</a>
+    </div>
+</main>
+</body>
+</html>

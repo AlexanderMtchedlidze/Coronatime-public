7a16219ee9412f9b6984fc1bba3e037b04bf0de2
AlexanderMtchedlidze <mtchedlidzealexander@gmail.com>
2023-04-25 13:25:01 +0400

    fix(typo): another type

--- resources/views/auth/password/feedback-email.blade.php
+++ resources/views/auth/password/feedback-email.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
-    <x-_email-header>
+    <x-_email-notice-header>
         <x-slot:heading>{{ trans('reset-password.success') }}</x-slot:heading>
         <x-form.auth.submit-button />
-    </x-_email-header>
+    </x-_email-notice-header>
 </x-layout>
